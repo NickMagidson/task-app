4c1d01124c036389ce7511dd26059244f88c9a1d
Nick <nickmagi24@gmail.com>
2021-10-26 10:15:21 -0400

    Component imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Overview from './Overview.';
 
 function App() {
   return (
---
+++ src/Overview.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Overview extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+
+
+
+}
