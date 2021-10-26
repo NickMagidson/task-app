b166eff3b11e731138bd95e5b647d82eaf45571e
Nick <nickmagi24@gmail.com>
2021-10-26 11:11:25 -0400

    handleChange and onSubmitTask

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,26 +7,46 @@ class App extends Component {
 
     this.state = {
       task: { text: ''},
-      task: [],
+      tasks: [],
     };
   }
 
-  
+  // Sets current task in state to whatever is typed in form.
+  handleChange = (e) => {
+    this.setState({
+      task : {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSumbitTask = (e) => {
+    e.preventDefault(); // To avoid refreshing the form after submitting
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task), // Adds task into the task array
+      task: { text: '' },
+    })
+  }
 
   render() {
     const { task, tasks } = this.state;
 
     return(
       <div>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
+        <form onSubmit={this.onSumbitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input 
+            onChange={this.handleChange}
+            value={task.text}
+            type="text" 
+            id="taskInput"
+          />
+          <button type="submit">
+            Add Task
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
 
